9ae8074667f175b3fc43978a84af7b3800517085
Ian Smith <iansmith@acm.org>
2014-07-22 08:38:31 -0700

    This is a major change in the nomenclature of pickett.
    These changes make the algorithms and documentation much better, but
    there are little functional changes.

--- io/doc.go
+++ io/doc.go
@@ -1,3 +1,5 @@
 //This package is here to allow mocks to be generated of all the calls that touch the
 //filesystem or docker.  With these, it is easy to write unit tests of the pickett logic.
+//The interfaces in this package are designed to be independent and thus do not
+//call each other.
 package io
