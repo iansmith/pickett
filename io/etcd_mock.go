169e1bdbd1a33cc3d7444666ef69ef5f0ece053a
Theodore Nordsieck <theodore.nordsieck@gmail.com>
2014-08-19 11:52:30 -0700

    tests work

--- io/etcd_mock.go
+++ io/etcd_mock.go
@@ -61,3 +61,14 @@ func (_m *MockEtcdClient) Del(_param0 string) (string, error) {
 func (_mr *_MockEtcdClientRecorder) Del(arg0 interface{}) *gomock.Call {
 	return _mr.mock.ctrl.RecordCall(_mr.mock, "Del", arg0)
 }
+
+func (_m *MockEtcdClient) Children(_param0 string) ([]string, error) {
+	ret := _m.ctrl.Call(_m, "Children", _param0)
+	ret0, _ := ret[0].([]string)
+	ret1, _ := ret[1].(error)
+	return ret0, ret1
+}
+
+func (_mr *_MockEtcdClientRecorder) Children(arg0 interface{}) *gomock.Call {
+	return _mr.mock.ctrl.RecordCall(_mr.mock, "Children", arg0)
+}
